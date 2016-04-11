4d1b3ef388e5df5ea972c1f9dfa6144050490f70
Elliot Saba <staticfloat@gmail.com>
2016-04-11 14:58:49 -0700

    commit

--- commit.py
+++ commit.py
@@ -5,5 +5,5 @@ with open("output.txt", "r+") as f:
     f.seek(0)
     for line in lines:
         if len(line) > 8:
-            line = line[:-8] + chr(ord(line[-7]) + 1) + line[-7:]
+            line = line[:-7] + chr(ord(line[-7]) + 1) + line[-6:]
         f.write(line)
